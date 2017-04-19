825294b67cda5500ee1eeaf6265cce4552ee03e9
Loïc <loic@jeedom.com>
2017-04-19 12:24:02 +0200

    improve code

--- core/class/ipx800v4.class.php
+++ core/class/ipx800v4.class.php
@@ -223,36 +223,7 @@ class ipx800v4Cmd extends cmd {
 		$eqLogic = $this->getEqLogic();
 		$url = 'http://' . $eqLogic->getConfiguration('ip') . '/api/xdevices.json?key=' . $eqLogic->getConfiguration('apikey');
 		$url .= '&' . $this->getConfiguration('actionCmd') . $this->getConfiguration('actionArgument');
-		if ($this->getConfiguration('actionArgument') == 'VA') {
-			if (strlen($this->getConfiguration('actionParameter' . $this->getConfiguration('actionArgument'))) == 1) {
-				$url .= '0' . $this->getConfiguration('actionParameter' . $this->getConfiguration('actionArgument'));
-			} else {
-				$url .= $this->getConfiguration('actionParameter' . $this->getConfiguration('actionArgument'));
-			}
-			$value = $this->getConfiguration('actionOption' . $this->getConfiguration('actionArgument'));
-			switch ($this->getSubType()) {
-				case 'slider':
-					if (trim($value) == '') {
-						$value = '#slider#';
-					}
-					$value = str_replace('#slider#', urlencode($_options['slider']), $value);
-					break;
-				case 'color':
-					if (trim($value) == '') {
-						$value = '#color#';
-					}
-					$value = str_replace('#color#', urlencode($_options['color']), $value);
-					break;
-				case 'message':
-					if (trim($value) == '') {
-						$value = '#title# #message#';
-					}
-					$value = str_replace('#title#', urlencode($_options['title']), $value);
-					$value = str_replace('#message#', urlencode($_options['message']), $value);
-					break;
-			}
-			$url .= '=' . $value;
-		} else if ($this->getConfiguration('actionArgument') == 'C') {
+		if ($this->getConfiguration('actionArgument') == 'VA' || $this->getConfiguration('actionArgument') == 'C' || $this->getConfiguration('actionArgument') == 'VR') {
 			if (strlen($this->getConfiguration('actionParameter' . $this->getConfiguration('actionArgument'))) == 1) {
 				$url .= '0' . $this->getConfiguration('actionParameter' . $this->getConfiguration('actionArgument'));
 			} else {
