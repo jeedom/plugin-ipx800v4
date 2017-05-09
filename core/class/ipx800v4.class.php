8141ee645089d6e7f560f819cd743e80132249d0
Loïc <loic@jeedom.com>
2017-05-09 11:09:05 +0200

    improve code

--- core/class/ipx800v4.class.php
+++ core/class/ipx800v4.class.php
@@ -106,45 +106,17 @@ class ipx800v4 extends eqLogic {
 
 	public function getIPXinfo() {
 		$return = array();
-		$url = 'http://' . $this->getConfiguration('ip') . '/api/xdevices.json?key=' . $this->getConfiguration('apikey') . '&Get=all';
-		$request_http = new com_http($url);
-		try {
-			$result = $request_http->exec();
-			if (is_json($result)) {
-				$return = array_merge($return, json_decode($result, true));
-			}
-		} catch (Exception $e) {
+		foreach (array('all', 'A', 'VA', 'C') as $get) {
+			$url = 'http://' . $this->getConfiguration('ip') . '/api/xdevices.json?key=' . $this->getConfiguration('apikey') . '&Get=' . $get;
+			$request_http = new com_http($url);
+			try {
+				$result = $request_http->exec();
+				if (is_json($result)) {
+					$return = array_merge($return, json_decode($result, true));
+				}
+			} catch (Exception $e) {
 
-		}
-		$url = 'http://' . $this->getConfiguration('ip') . '/api/xdevices.json?key=' . $this->getConfiguration('apikey') . '&Get=A';
-		$request_http = new com_http($url);
-		try {
-			$result = $request_http->exec();
-			if (is_json($result)) {
-				$return = array_merge($return, json_decode($result, true));
 			}
-		} catch (Exception $e) {
-
-		}
-		$url = 'http://' . $this->getConfiguration('ip') . '/api/xdevices.json?key=' . $this->getConfiguration('apikey') . '&Get=VA';
-		$request_http = new com_http($url);
-		try {
-			$result = $request_http->exec();
-			if (is_json($result)) {
-				$return = array_merge($return, json_decode($result, true));
-			}
-		} catch (Exception $e) {
-
-		}
-		$url = 'http://' . $this->getConfiguration('ip') . '/api/xdevices.json?key=' . $this->getConfiguration('apikey') . '&Get=C';
-		$request_http = new com_http($url);
-		try {
-			$result = $request_http->exec();
-			if (is_json($result)) {
-				$return = array_merge($return, json_decode($result, true));
-			}
-		} catch (Exception $e) {
-
 		}
 		log::add('ipx800v4', 'debug', 'IPX800 ' . $this->getConfiguration('ip') . ' info  : ' . json_encode($return));
 		return $return;
